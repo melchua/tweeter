5790778552a3bed6a2df5b4a5e2ee2dff5568460
mel <mchuam@gmail.com>
2018-04-26 23:33:29 +0000

    modified getTweets to allow access to mongodb

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -17,11 +17,12 @@ module.exports = function makeDataHelpers(db) {
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      db.collection("tweets").find().toArray(function(err, tweetArray) {
+        callback(null, tweetArray.sort(sortNewestFirst));
       });
     }
 
   };
 }
+
