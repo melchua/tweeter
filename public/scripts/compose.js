ae37785abb13e9b44507fcad002286a2a04a472e
mel <mchuam@gmail.com>
2018-04-27 00:18:04 +0000

    added comments into index.html and cleaned up white space

--- public/scripts/compose.js
+++ public/scripts/compose.js
@@ -1,6 +1,5 @@
 $(function() {
-  // toggle slide
-
+  // This function is used to toggle slide upon click of Compose button
   const $newTweet = $(".new-tweet");
   $(".compose").on("click", function(event) {
     event.preventDefault();
